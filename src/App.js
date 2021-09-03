6e58d86094e485d3144a1610e2514e83c12a3dfc
rodrigo luiz <rodrigoluizandradedeoliveira@gmail.com>
2021-09-02 23:02:21 -0300

    inicial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState(new Array());
+
+  const addTodo = (newTodo) => {};
+
+  const handleTodo = () => {};
+
+  return <div className="App-header"></div>;
+}
+
+export default App;
